d6861ee7f3e0c7655fea998cdeb5cdb8173ed205
TrapFish <92753280+TrapFish@users.noreply.github.com>
2024-07-01 00:17:34 +0530

    Logic for the Kth group
    Kth logic

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-console.log("Line 12221")
+//Flat array with the change in the K-1 value from the array 
+
+function flatArrayFromMatrix(mat){
+    let flatArray = mat.reduce((acc, val)=>{
+        if(Array.isArray(val)){
+            return acc.concat(...val)
+        } else {
+            return acc.concat(val)
+        }
+    } , []);
+    return flatArray;
+}
+
+function kthSmallest(mat, k){
+    let flatArrayRecieved = flatArrayFromMatrix(mat);
+    let sortedFlatArray = flatArrayRecieved.sort((a,b)=> a-b);
+    return sortedFlatArray[k-1]
+}
+
+
+const matrix = [
+    [10, 20, 30],
+    [15, 25, 35],
+    [5,  50, 60]
+  ];
+  
+  const k = 4; 
+  const result = kthSmallest(matrix, k);
+  console.log(`The ${k}th smallest element is: ${result}`);
